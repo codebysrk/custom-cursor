9079db3ede030441064822f66bb70ce612c38db1
Shahrukh Khan <153599461+codebysrk@users.noreply.github.com>
2025-12-24 13:14:10 +0530

    make some add-on

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const screen = document.querySelector("main");
-const span = document.querySelector("span");
-screen.addEventListener("mousemove", (event) => {
-  const x = event.clientX;
-  const y = event.clientY;
-  span.style.left = `${x+18}px`;
-  span.style.top = `${y+18}px`;
+const cursor = document.createElement("span");
+screen.appendChild(cursor);
+
+screen.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
 });
